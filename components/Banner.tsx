0bbc04fce1bcaa22c4dcde9ef4a9fafc0d9274f7
Elua-b <bugingoeloi@gmail.com>
2022-10-08 13:32:26 +0200

    adding fetched images

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Image  from 'next/image'
-function Banner() {
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { Movie } from "../typings";
+import { baseUrl } from "../constants/movie";
+interface Props {
+  netflixOriginals: Movie[];
+}
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+  console.log(movie);
+
   return (
     <div>
-    {/* <div>
-        <Image/>
-    </div> */}
+      <div className="relative  top-0 left-0 h-[95vh] w-screen">
+        <Image
+          layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          objectFit
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
