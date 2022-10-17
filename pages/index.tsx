8e7d3c2b8068b370b61bcf3ec9abfa64e0dbbe66
Elua-b <bugingoeloi@gmail.com>
2022-10-17 19:22:50 +0200

    loading feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({
 }: Props) => {
   // console.log(netflixOriginals);
   const {logout,loading}=useAuth()
-  if(loading) return nullcd 
+  if(loading) return null
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
